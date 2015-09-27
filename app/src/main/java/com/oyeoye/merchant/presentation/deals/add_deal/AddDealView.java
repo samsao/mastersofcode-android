f21692c565f67969cbda1ae2d2e1253d2bafc89e
Vincent Legault <vlegault@samsaodev.com>
2015-09-26 21:56:38 -0400

    Prepared logic to sent deal to backend

--- app/src/main/java/com/oyeoye/merchant/presentation/deals/add_deal/AddDealView.java
+++ app/src/main/java/com/oyeoye/merchant/presentation/deals/add_deal/AddDealView.java
@@ -3,7 +3,6 @@ package com.oyeoye.merchant.presentation.deals.add_deal;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,9 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.oyeoye.merchant.R;
 import com.oyeoye.merchant.business.camera.SimpleCamera;
@@ -42,9 +43,20 @@ public class AddDealView extends PresentedFrameLayout<AddDealPresenter> {
     public FrameLayout mCameraPreviewContainer;
     @Bind(R.id.screen_add_deal_take_picture_button)
     public Button mTakePictureButton;
+    @Bind(R.id.screen_add_deal_title)
+    public EditText mDealTitle;
+    @Bind(R.id.screen_add_deal_original_price)
+    public EditText mOriginalPrice;
+    @Bind(R.id.screen_add_deal_discounted_price)
+    public EditText mDiscountedPrice;
+    @Bind(R.id.screen_add_deal_description)
+    public EditText mDealDescription;
+    @Bind(R.id.screen_add_deal_broadcast_button)
+    public Button mBroadcastDealButton;
 
     private Activity mActivity;
     private SimpleCamera mSimpleCamera;
+    private ImageView mDealImageView;
 
     public AddDealView(Context context) {
         super(context);
@@ -53,6 +65,7 @@ public class AddDealView extends PresentedFrameLayout<AddDealPresenter> {
         View view = View.inflate(context, R.layout.screen_add_deal, this);
         ButterKnife.bind(view);
         setLayoutParams(new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        setupBroadcastDealButton();
     }
 
     @Override
@@ -117,6 +130,7 @@ public class AddDealView extends PresentedFrameLayout<AddDealPresenter> {
 
     private void setupTakePictureButton() {
         mTakePictureButton.setText(R.string.add_deal_take_picture);
+        mDealImageView = null;
         mTakePictureButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -137,8 +151,29 @@ public class AddDealView extends PresentedFrameLayout<AddDealPresenter> {
     }
 
     private void setDealPicture(Bitmap bitmap) {
-        ImageView imageView = new ImageView(mActivity);
-        imageView.setImageBitmap(bitmap);
-        mCameraPreviewContainer.addView(imageView);
+        mDealImageView = new ImageView(mActivity);
+        mDealImageView.setImageBitmap(bitmap);
+        mCameraPreviewContainer.addView(mDealImageView);
+    }
+
+    private void setupBroadcastDealButton() {
+        mBroadcastDealButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDealImageView == null) {
+                    Toast.makeText(mActivity, R.string.add_deal_must_take_picture, Toast.LENGTH_LONG).show();
+                } else if (mDealTitle.getText().length() <= 0 ||
+                        mOriginalPrice.getText().length() <= 0 ||
+                        mDiscountedPrice.getText().length() <= 0 ||
+                        mDealDescription.getText().length() <= 0) {
+                    Toast.makeText(mActivity, R.string.add_deal_fill_all_fields, Toast.LENGTH_LONG).show();
+                } else {
+                    mTakePictureButton.setEnabled(false);
+                    mBroadcastDealButton.setEnabled(false);
+                    //TODO send au icitte criss
+                    Toast.makeText(mActivity, "TODO : broadcast ton pelvish", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
